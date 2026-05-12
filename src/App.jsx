ca0368c387081274c8d08a4813edd8a3a94c20e4
Sanjievkumar <murugansanjievkumar@gmail.com>
2026-05-12 14:52:47 +0530

    Fix: Added ScrollToTop utility for seamless page transitions

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import ScrollToTop from './components/ScrollToTop';
 import Home from './pages/Home';
 import About from './pages/About';
 
 function App() {
   return (
     <Router>
+      <ScrollToTop />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
