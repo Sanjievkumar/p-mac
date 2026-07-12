9147e54d7813d294e7050fc33d86fc698ff85b6b
Sanjievkumar <murugansanjievkumar@gmail.com>
2026-07-12 20:32:17 +0530

    style: Remove video play button overlay from consultancy hero image

--- src/pages/Consultancy.jsx
+++ src/pages/Consultancy.jsx
@@ -614,21 +614,13 @@ function DesignPerformance() {
             </div>
           </div>
 
-          {/* Right Column: Image & Media */}
-          <div className="w-full lg:w-[55%] relative min-h-[400px] lg:min-h-full overflow-hidden group cursor-pointer">
+          {/* Right Column: Image */}
+          <div className="w-full lg:w-[55%] relative min-h-[400px] lg:min-h-full overflow-hidden group">
             <img 
               src="/images/consultancy/facility-split.png" 
               alt="Facility Split View" 
               className="absolute inset-0 w-full h-full object-cover transition-transform duration-700 group-hover:scale-105"
             />
-            
-            {/* Play Button Overlay */}
-            <div className="absolute inset-0 bg-black/5 group-hover:bg-black/10 transition-colors duration-300" />
-            <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-20 h-20 bg-white rounded-full flex items-center justify-center shadow-[0_10px_30px_rgba(0,0,0,0.15)] transition-transform duration-300 group-hover:scale-110">
-              <svg className="w-8 h-8 text-[#E31E24] ml-1" fill="currentColor" viewBox="0 0 24 24">
-                <path d="M8 5v14l11-7z" />
-              </svg>
-            </div>
           </div>
         </motion.div>
       </div>
